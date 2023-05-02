7b1461bb00308cbb7dbbf0e5298b27239f365add
Дмитрий <shcheklein.dev@gmail.com>
2023-05-02 10:37:12 +0300

    2.11. Добавит сущность BlogUserEntity.

---
+++ project/apps/users/src/app/blog-user/blog-user.entity.ts
@@ -0,0 +1,27 @@
+import { User, UserRole } from '@project/shared/shared-types';
+
+export class BlogUserEntity implements User {
+  public _id?: string;
+  public avatar: string;
+  public dateBirth: Date;
+  public email: string;
+  public firstName: string;
+  public lastName: string;
+  public passwordHash: string;
+  public role: UserRole;
+
+  constructor(blogUser: User) {
+    this.fillEntity(blogUser);
+  }
+
+  public fillEntity(blogUser: User) {
+    this._id = blogUser._id;
+    this.avatar = blogUser.avatar;
+    this.dateBirth = blogUser.dateBirth;
+    this.email = blogUser.email;
+    this.firstName = blogUser.firstName;
+    this.lastName = blogUser.lastName;
+    this.passwordHash = blogUser.passwordHash;
+    this.role = blogUser.role;
+  }
+}
--- project/libs/shared/shared-types/src/lib/user.interface.ts
+++ project/libs/shared/shared-types/src/lib/user.interface.ts
@@ -3,9 +3,9 @@ import { UserRole } from './user-role.enum.js';
 export interface User {
   _id?: string;
   email: string;
-  firstname: string;
-  lastname: string;
-  dateVirth: Date;
+  firstName: string;
+  lastName: string;
+  dateBirth: Date;
   avatar: string;
   passwordHash: string;
   role: UserRole;
