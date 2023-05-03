936546624e533db350a0beba0b10f13405b80727
Дмитрий <shcheklein.dev@gmail.com>
2023-05-03 10:15:46 +0300

    2.17. Добавит импорт модуля BlogUserModule.

--- project/apps/users/src/app/authentication/authentication.module.ts
+++ project/apps/users/src/app/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { BlogUserModule } from '../blog-user/blog-user.module.js';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
