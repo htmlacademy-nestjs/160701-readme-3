ea34044c9e416e7b1b0fa96dbaff837395819837
Dmitry <sheklein.dima@gmail.com>
2023-05-03 20:20:52 +0300

    2.29. Добавит базовую настройку @nestjs/swagger.

--- project/apps/users/src/main.ts
+++ project/apps/users/src/main.ts
@@ -3,14 +3,22 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-
-import { AppModule } from "./app/app.module";
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
+  const config = new DocumentBuilder()
+    .setTitle('The «Users» service')
+    .setDescription('Users service API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
