a97e734bacd480001f8bf76eae2636067d2d59e3
Дмитрий <shcheklein.dev@gmail.com>
2023-05-03 11:05:12 +0300

    2.26. Добавит обработчики маршрутов /login, /:id.

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { fillObject } from '@project/util/util-core';
 import { UserRdo } from './rdo/user.rdo.js';
+import { LoggedUserRdo } from './rdo/logged-user.rdo.js';
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
@@ -12,5 +13,19 @@ export class AuthenticationController {
     const newUser = await this.authService.register(dto);
 
     return fillObject(UserRdo, newUser);
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoggedUserRdo) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+
+    return fillObject(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+
+    return fillObject(UserRdo, existUser);
   }
 }
