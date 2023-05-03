1891efb42c5c3e03d2327c0f8f56bc67b2f20453
Дмитрий <shcheklein.dev@gmail.com>
2023-05-03 11:02:46 +0300

    2.25. Добавит обработчик маршрута /register.

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service.js';
-
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { fillObject } from '@project/util/util-core';
+import { UserRdo } from './rdo/user.rdo.js';
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+
+    return fillObject(UserRdo, newUser);
+  }
 }
