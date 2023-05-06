8e0ea8623fec3c5d2cd114189ec63bb3879aca57
Дмитрий <shcheklein.dev@gmail.com>
2023-05-06 19:17:23 +0300

    3.15. Добавит функцию getMongoConnectionString.

--- project/libs/util/util-core/src/lib/helpers.ts
+++ project/libs/util/util-core/src/lib/helpers.ts
@@ -5,3 +5,14 @@ export function fillObject<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
     excludeExtraneousValues: true,
   });
 }
+
+export function getMongoConnectionString({
+  userName,
+  password,
+  host,
+  port,
+  databaseName,
+  authDatabase,
+}): string {
+  return `mongodb://${userName}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+}
