80a6833169245d79b7fe47b425eafad86833f007
Дмитрий <shcheklein.dev@gmail.com>
2023-05-06 17:40:35 +0300

    3.8. Извлекаем данные из ConfigService.

--- project/libs/config/config-users/src/lib/app.config.ts
+++ project/libs/config/config-users/src/lib/app.config.ts
@@ -4,18 +4,18 @@ import * as Joi from 'joi';
 const DEFAULT_PORT = 3000;
 
 export interface ApplicationConfig {
-  enviroment: string;
+  environment: string;
   port: number;
 }
 
 export default registerAs('application', (): ApplicationConfig => {
   const config: ApplicationConfig = {
-    enviroment: process.env.NODE_ENV,
+    environment: process.env.NODE_ENV,
     port: parseInt(process.env.PORT || DEFAULT_PORT.toString(), 10),
   };
 
   const validationSchema = Joi.object<ApplicationConfig>({
-    enviroment: Joi.string()
+    environment: Joi.string()
       .valid('development', 'production', 'stage')
       .required(),
     port: Joi.number().port().default(DEFAULT_PORT),
@@ -25,7 +25,7 @@ export default registerAs('application', (): ApplicationConfig => {
 
   if (error) {
     throw new Error(
-      `[ApplicationConfig]: Enviroments validation failed.Please check .env file. Error message: Mongo.${error.message}`
+      `[ApplicationConfig]: environments validation failed.Please check .env file. Error message: Mongo.${error.message}`
     );
   }
 
