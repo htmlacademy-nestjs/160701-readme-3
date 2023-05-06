ef203ee8c27a07662b3f29cb74233d4c991fa2ea
Дмитрий <shcheklein.dev@gmail.com>
2023-05-06 20:17:41 +0300

    Fix: BlogUserModel

--- project/apps/users/src/app/blog-user/blog-user.model.ts
+++ project/apps/users/src/app/blog-user/blog-user.model.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({
-  collection: 'readme-users',
+  collection: 'users',
   timestamps: true,
 })
 export class BlogUserModel extends Document implements User {
