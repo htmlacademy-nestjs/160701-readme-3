d5e9c64ccc2d211bca4b4f8e29e3dac2f5ca815c
Дмитрий <shcheklein.dev@gmail.com>
2023-05-07 15:11:54 +0300

    4.18. Добавит интерфейсы для сущностей блога.

--- project/libs/shared/shared-types/src/lib/post.interface.ts
+++ project/libs/shared/shared-types/src/lib/post.interface.ts
@@ -1,7 +1,13 @@
 import { PostType } from './post-type.enum.js';
+import { Comment } from './comment.interface';
 
 export interface Post {
   _id?: string;
   tag?: string[];
   type: PostType;
+  createdAt?: Date;
+  publishAt?: Date;
+  comments: Comment[];
+  userId: string;
+  content: string;
 }
