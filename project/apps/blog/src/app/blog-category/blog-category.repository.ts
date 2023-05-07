3fa8d1cf699a544303698d5b8d852e0e97c8749f
Дмитрий <shcheklein.dev@gmail.com>
2023-05-07 15:37:11 +0300

    4.21. Добавит BlogCategoryRepository.

---
+++ project/apps/blog/src/app/blog-category/blog-category.repository.ts
@@ -0,0 +1,59 @@
+import { CRUDRepository } from '@project/util/util-types';
+import { BlogCategoryEntity } from './blog-category.entity';
+import { Category } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class BlogCategoryRepository
+  implements CRUDRepository<BlogCategoryEntity, number, Category>
+{
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async create(item: BlogCategoryEntity): Promise<Category> {
+    return this.prisma.category.create({
+      data: { ...item.toObject() },
+    });
+  }
+
+  public async destroy(categoryId: number): Promise<void> {
+    await this.prisma.category.delete({
+      where: {
+        categoryId,
+      },
+    });
+  }
+
+  public findById(categoryId: number): Promise<Category | null> {
+    return this.prisma.category.findFirst({
+      where: {
+        categoryId,
+      },
+    });
+  }
+
+  public async find(ids: number[] = []): Promise<Category[]> {
+    return this.prisma.category.findMany({
+      where: {
+        categoryId: {
+          in: ids.length > 0 ? ids : undefined,
+        },
+      },
+    });
+  }
+
+  public update(
+    categoryId: number,
+    item: BlogCategoryEntity
+  ): Promise<Category> {
+    return this.prisma.category.update({
+      where: {
+        categoryId,
+      },
+      data: {
+        ...item.toObject(),
+        categoryId,
+      },
+    });
+  }
+}
