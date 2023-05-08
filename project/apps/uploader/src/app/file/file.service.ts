8d337157e847b015948e3260726e98a05b629b3d
Дмитрий <shcheklein.dev@gmail.com>
2023-05-08 16:36:40 +0300

    Fix: FileService.

--- project/apps/uploader/src/app/file/file.service.ts
+++ project/apps/uploader/src/app/file/file.service.ts
@@ -11,9 +11,9 @@ export class FileService {
     private readonly applicationConfig: ConfigType<typeof uploaderConfig>
   ) {}
 
-  public async writeFile(file: ExpressAdapter.Multer.File): Promise<string> {
+  public async writeFile(file: Express.Multer.File): Promise<string> {
     const uploadDirectoryPath = this.applicationConfig.uploadDirectory;
-    const destinationFile = `${uploadDirectoryPath}/${file.orinalName}`;
+    const destinationFile = `${uploadDirectoryPath}/${file.originalname}`;
 
     await ensureDir(uploadDirectoryPath);
     await writeFile(destinationFile, file.buffer);
