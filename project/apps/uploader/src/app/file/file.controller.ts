5569525552e4926aeb5cea88892c38719ff697c6
Дмитрий <shcheklein.dev@gmail.com>
2023-05-08 16:43:58 +0300

    5.6. Добавит контроллер FileController. WIP

---
+++ project/apps/uploader/src/app/file/file.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileService } from './file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FileController {
+  constructor(private readonly fileService: FileService) {}
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileService.writeFile(file);
+  }
+}
