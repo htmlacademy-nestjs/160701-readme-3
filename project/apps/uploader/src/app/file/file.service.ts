3855ffa24478f6584c595125576b3b0770965833
Дмитрий <shcheklein.dev@gmail.com>
2023-05-08 17:36:56 +0300

    5.12. Добавит уникальные имена для файлов.

--- project/apps/uploader/src/app/file/file.service.ts
+++ project/apps/uploader/src/app/file/file.service.ts
@@ -5,6 +5,9 @@ import 'multer';
 import { ensureDir } from 'fs-extra';
 import { writeFile } from 'node:fs/promises';
 import dayjs from 'dayjs';
+import crypto from 'node:crypto';
+import { extension } from 'mime-types';
+
 export class FileService {
   constructor(
     @Inject(uploaderConfig.KEY)
@@ -14,8 +17,12 @@ export class FileService {
   public async writeFile(file: Express.Multer.File): Promise<string> {
     const [year, month] = dayjs().format('YYYY MM').split(' ');
     const { uploadDirectory } = this.applicationConfig;
+
+    const filename = crypto.randomUUID();
+    const fileExtension = extension(file.mimetype);
+
     const uploadDirectoryPath = `${uploadDirectory}/${year}/${month}`;
-    const destinationFile = `${uploadDirectoryPath}/${file.originalname}`;
+    const destinationFile = `${uploadDirectoryPath}/${filename}.${fileExtension}`;
 
     await ensureDir(uploadDirectoryPath);
     await writeFile(destinationFile, file.buffer);
